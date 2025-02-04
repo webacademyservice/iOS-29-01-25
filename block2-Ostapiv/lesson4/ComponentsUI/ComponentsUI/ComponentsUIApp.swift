872901b70b2d67df9df4bb637ed50a9798af8526
DenisOstapiv <denisostapiv@gmail.com>
2025-02-04 13:49:50 +0200

    Add block 2,  lesson3-4 Ostapiv

---
+++ block2-Ostapiv/lesson4/ComponentsUI/ComponentsUI/ComponentsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsUIApp.swift
+//  ComponentsUI
+//
+//  Created by Denis Ostapiv on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
