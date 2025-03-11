6d05f6b78ec951677981c3b28cf93f5816e4f7b1
denisostapiv <136182197+DenisOstapiv@users.noreply.github.com>
2025-03-11 21:43:53 +0200

    Update row view

--- Ostapiv-HW/Final-project/SpotifyListPodcast/SpotifyListPodcast/Sources/View/ContentView.swift
+++ Ostapiv-HW/Final-project/SpotifyListPodcast/SpotifyListPodcast/Sources/View/ContentView.swift
@@ -14,70 +14,71 @@ struct ContentView: View {
         VStack {
             
             if viewModel.rows.count > 0 {
-                
-                List(viewModel.rows, id: \.title) { row in
-                    VStack {
-                        HStack{
-                            switch row.image{
-                            case .remoute(let url):
-                                AsyncImage(url: url) { image in
-                                    image.resizable()
+                NavigationStack {
+                    List(viewModel.rows, id: \.title) { row in
+                        VStack(spacing:10) {
+                            HStack(spacing:12){
+                                switch row.image{
+                                case .remoute(let url):
+                                    AsyncImage(url: url) { image in
+                                        image.resizable()
+                                            .scaledToFit()
+                                            .frame(width: 50, height: 50)
+                                            .cornerRadius(4)
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                case .local(let imageName):
+                                    Image(imageName) //інша іконка для відсутнього зображення
+                                        .resizable()
                                         .scaledToFit()
                                         .frame(width: 50, height: 50)
-                                        .cornerRadius(4)
-                                } placeholder: {
-                                    ProgressView()
                                 }
-                            case .local(let imageName):
-                                Image(imageName) //інша іконка для відсутнього зображення
+                                Text(row.title)
+                                    .font(.system(size: 18))
+                                Spacer()
+                            }
+                            HStack{
+                                Text(row.description)
+                                    .font(.system(size: 12))
+                                    .lineLimit(2)
+                                    .foregroundColor(Color(red: 98/255.0, green: 98/255.0, blue: 98/255.0))
+                            }
+                            HStack(spacing:12){
+                                Image(systemName: "plus.circle")
                                     .resizable()
                                     .scaledToFit()
-                                    .frame(width: 50, height: 50)
+                                    .frame(width: 24, height: 24)
+                                Image(systemName: "square.and.arrow.down")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 24, height: 24)
+                                Image(systemName: "square.and.arrow.up")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 24, height: 24)
+                                Image(systemName: "ellipsis")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 24, height: 24)
+                                Spacer()
+                                Text("\(row.duration) хв")
+                                    .font(.system(size: 14))
                             }
                             
-                            Text(row.title)
-                                .font(.system(size: 16))
-                            Spacer()
                         }
-                        Text(row.description)
-                            .font(.system(size: 12))
-                            .lineLimit(2)
-                            .foregroundColor(Color(red: 98/255.0, green: 98/255.0, blue: 98/255.0))
-                        HStack{
-                            Image(systemName: "plus.circle")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 22, height: 22)
-                            Image(systemName: "arrow.down.app")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 22, height: 22)
-                            Image(systemName: "square.and.arrow.up")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 22, height: 22)
-                            Image(systemName: "ellipsis")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 22, height: 22)
-                            Spacer()
-                            Text("Тривалість \(row.duration) хв")
-                                .font(.system(size: 14))
-                            
-                        }
-                        
+                        .listRowInsets(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
                     }
-                    
+                    .listStyle(.plain) // Видаляє стандартний стиль списку
+                    .navigationTitle("List Podcasts") // Заголовок
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)  // Це забезпечить, щоб List заповнював весь доступний простір
-                .listStyle(PlainListStyle()) // Задає простий стиль списку
-                
+                .edgesIgnoringSafeArea(.all)
                 
             } else {
                 Text("Завантаження...")
             }
         }
-//        .padding()
+        //        .padding()
         .onAppear {
             viewModel.queryChange()
         }
